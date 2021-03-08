1271c31a4ca29eebce3fe956f21a9b442d040df2
Fernando De Leon <gasnare@hotmail.com>
2021-03-07 21:43:41 -0500

    Modulo OS para manejo de servicios de servidores

---
+++ modulos/os.js
@@ -0,0 +1,11 @@
+const os = require('os');
+
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.cpus().length);
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.tmpdir());
+console.log(os.hostname());
+console.log(os.networkInterfaces());
